154f7b5e1d123f84d25d195a03edee2853dd6387
VicenteCZ0 <vicente_castro_zepeda@hotmail.com>
2023-09-25 23:06:20 -0300

    menu
    madafakin menu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 #include <iostream>
 
 int main (){
-    std::cout <<"hola mundo";
-    std::cin.get();
+    cout <<"hola mundo";
+    cin.get();
     return 0;
+
+    int opc
+    do
+    {
+        switch (opc)
+        {
+        case 1:
+            cout <<"[1] Anterior";
+            break;
+        case 2:
+            cout <<"[2] Siguiente";
+            break;
+        case 3:
+            cout <<"[3] Agregar";
+            break;
+        case 4:
+            cout <<"[4] eliminar";
+            break;
+        case 5:
+            cout <<"[5] salir";
+            break;
+
+        
+        
+        }
+    } 
 }
